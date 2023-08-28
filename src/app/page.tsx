270f2e6ce93d3c49f38fb2d9b3bfa59b84d3ce68
Kabina Thapa <thapakabina28@gmail.com>
2023-08-28 17:31:49 -0400

    chore(products):organize top trending products data into separate files for separation of concern

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,71 +17,7 @@ import images from '@/static-data/images'
  
 //On Sale products
 
-export const toptrending=[
-  {
-    id:213,
-    size:'small',
-    subcategory:'Tshirt',
-    category:'men',
-    image:'https://minion-vinovatheme.myshopify.com/cdn/shop/products/5_600x.jpg?v=1613984550',
-    
-    name:'Light green simpsons T-shirt',
-    price:45.00
-  },
-    {
-      id:214,
-      size:'large',
-      subcategory:'Pants',
-    category:'men',
-    image:'https://minion-vinovatheme.myshopify.com/cdn/shop/products/3_8577e617-d687-43a4-bd96-d1950e73ecaf_600x.jpg?v=1614067794',
-     
-    name:'Classic one-breasted jacket',
-    price:26.00
-    },
-    {
-      id:215,
-      size:'large',
-      subcategory:'Tshirt',
-    category:'women',
-      image:'https://minion-vinovatheme.myshopify.com/cdn/shop/products/1_5cb5d1e1-2c31-499b-9010-ccac2fecf49e_600x.jpg?v=1615530706', 
-   
-    name:'Pink long Blazor',
-    price:40.00
-    },
-    {
-      id:216,
-      size:'small',
-      subcategory:'Tshirt',
-    category:'women',
-    image:'https://minion-vinovatheme.myshopify.com/cdn/shop/products/1_81452fd6-1818-4c32-923b-302b3d879a3a_600x.jpg?v=1615531056',
-     
-    name:'Organic cotton oversize blazor',
-    price:78.00
-    },
-    
-    {
-    id:217,
-    size:'small',
-    subcategory:'Tshirt',
-    category:'men',
-     image:'https://minion-vinovatheme.myshopify.com/cdn/shop/products/1_0ae182fa-8c71-468e-850b-23fc81cb3bf4_600x.jpg?v=1613984721',
-  
-    name:'Blue Hoodie with zip',
-    price:32.00
-    },
-    
-    
-    {
-      id:218,
-      size:'large',
-      subcategory:'Tshirt',
-    category:'women',
-    image:'https://minion-vinovatheme.myshopify.com/cdn/shop/products/2_e64bc63a-735c-4c2c-9542-81681cff4891_540x.jpg?v=1614070027',
-    
-    name:'Faux suede biker jacket',
-    price:60.00
-}
-]
+
 export default function Home() {
   const {item, status, error} = useSelector((state:RootState)=>state.category);
   const items=useSelector((state:RootState)=>state.wishlist.item)
