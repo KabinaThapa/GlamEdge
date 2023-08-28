78ee8b37f917a63033f9a0900628c99c42211804
Kabina Thapa <thapakabina28@gmail.com>
2023-08-28 17:30:57 -0400

    chore(products):organize onsale products data into separate files for separation of concern

---
+++ src/static-data/onsale.ts
@@ -0,0 +1,65 @@
+export const onsale=[
+    {
+      id:207,
+      size:'small',
+      subcategory:'Tshirt',
+      category:'women',
+      image:'https://minion-vinovatheme.myshopify.com/cdn/shop/products/1_4b6df0cc-511d-4f31-8475-08b1346b1921_1120x.jpg?v=1614072211',
+      
+      name:'HIGH TURTLENECK JUMPER',
+      price:45.00
+    },
+      {
+        id:208,
+        size:'large',
+        subcategory:'Pants',
+      category:'women',
+      image:'https://minion-vinovatheme.myshopify.com/cdn/shop/products/4_bf88b3aa-813f-423a-8e55-46fee7f68db9_1120x.jpg?v=1614073956',
+       
+      name:'Basic sweat joggers',
+      price:26.00
+      },
+      {
+        id:209,
+        size:'large',
+        subcategory:'Tshirt',
+      category:'women',
+        image:'https://skudmart.myshopify.com/cdn/shop/products/s-p-8-1_grande.jpg?v=1569978781', 
+     
+      name:'Strappy Summer Beach Floral Flared',
+      price:40.00
+      },
+      {
+        id:210,
+        size:'small',
+        subcategory:'Tshirt',
+      category:'women',
+      image:'https://minion-vinovatheme.myshopify.com/cdn/shop/products/2_65b663d9-5f7a-4a9c-905a-2679b7edf4db_360x.jpg?v=1614073469',
+       
+      name:'Loose fit blazor and trouser set',
+      price:78.00
+      },
+      
+      {
+      id:211,
+      size:'small',
+      subcategory:'Tshirt',
+      category:'men',
+       image:'https://minion-vinovatheme.myshopify.com/cdn/shop/products/1_9bf4a17f-966d-4386-a7f4-424ce07c310d_1120x.jpg?v=1613984616',
+    
+      name:'Long-Sleeved Shirt',
+      price:32.00
+      },
+      
+      
+      {
+        id:212,
+        size:'large',
+        subcategory:'Tshirt',
+      category:'men',
+      image:'https://websitedemos.net/t-shirts-store-04/wp-content/uploads/sites/1115/2022/07/product-10-a.jpg',
+      
+      name:'Cotton white t-shirt',
+      price:24.00
+  }
+  ]
