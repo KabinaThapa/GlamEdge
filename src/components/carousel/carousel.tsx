edbaeec41bf2c7b50919d9dd704609b0ac2ff95e
Kabina Thapa <thapakabina28@gmail.com>
2023-09-26 21:41:28 -0400

    fix: fix issues with carousel component

--- src/components/carousel/carousel.tsx
+++ src/components/carousel/carousel.tsx
@@ -1,39 +1,37 @@
-import React from 'react'
-
+import React, { MouseEventHandler, ReactNode } from 'react'; // Import ReactNode
 
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css'
-import {HiChevronRight,HiChevronLeft} from 'react-icons/hi2'
+import 'slick-carousel/slick/slick-theme.css';
+import { HiChevronRight, HiChevronLeft } from 'react-icons/hi2';
 
-const NextArrow=({onClick})=>(
-  <button onClick ={onClick} className='w-10 h-10 bg-wenge hover:scale-110 rounded-full p-2  flex items-center absolute right-0 top-44'>
-  <HiChevronRight size={28} style={{color:'white'}}/>
+// Custom next arrow component
+const NextArrow = ({ onClick }: { onClick: MouseEventHandler}) => (
+  <button onClick={onClick} className='w-10 h-10 bg-wenge hover:scale-110 rounded-full p-2  flex items-center absolute right-0 top-44'>
+    <HiChevronRight size={28} style={{ color: 'white' }} />
   </button>
-)
-const PrevArrow=({onClick})=>(
-  <button onClick ={onClick} className='w-10 h-10 bg-wenge hover:scale-110 rounded-full p-2  flex items-center absolute left-0 z-[100] top-44'>
-  <HiChevronLeft size={28} style={{color:'white'}}/>
+);
+
+// Custom previous arrow component
+const PrevArrow = ({ onClick }: { onClick: MouseEventHandler}) => (
+  <button onClick={onClick} className='w-10 h-10 bg-wenge hover:scale-110 rounded-full p-2  flex items-center absolute left-0 z-[100] top-44'>
+    <HiChevronLeft size={28} style={{ color: 'white' }} />
   </button>
-)
+);
 
-const carousel = ({settings, children}) => {
-  const customSettings={
-    ...settings,
-    nextArrow:<NextArrow/>,
-    prevArrow: <PrevArrow/>,
-  }
- 
-
-  
-  
-  return (
-    
-      <Slider {...customSettings}>
-       {children}
-      </Slider>
-    
-  )
+interface CarouselProps {
+  settings: Record<string, any>;
+  children: ReactNode; // Specify children as ReactNode
 }
 
-export default carousel
+const Carousel = ({ settings, children }: CarouselProps) => {
+  const customSettings = {
+    ...settings,
+    nextArrow: <NextArrow onClick={settings.nextArrow}/>,
+    prevArrow: <PrevArrow onClick={settings.prevArrow} />,
+  };
+
+  return <Slider {...customSettings}>{children}</Slider>;
+};
+
+export default Carousel;
