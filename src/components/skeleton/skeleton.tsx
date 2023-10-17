2225a14cf931925b9042d35fa3863aee9c44e222
Kabina Thapa <thapakabina28@gmail.com>
2023-10-17 17:46:05 -0400

    fix: fix implement issues of skeleton loading

--- src/components/skeleton/skeleton.tsx
+++ src/components/skeleton/skeleton.tsx
@@ -2,11 +2,11 @@ import React from "react";
 
 const LoadingSkeleton = () => {
   return (
-    <div className="loading-skeleton flex justify-between w-full h-full p-[3%]">
+    <div className="loading-skeleton flex justify-between w-full h-screen p-[3%]">
       <div className="skeleton w-[60%] h-full"></div>
       <div className=" flex flex-col justify-between w-[30%] h-full ">
-      <div className="skeleton w-full h-80 " ></div>
-      <div className="skeleton  w-full h-80"></div>
+      <div className="skeleton w-full h-72 " ></div>
+      <div className="skeleton  w-full h-72"></div>
       </div>
     </div>
   );
